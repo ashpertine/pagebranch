a0b03df9a01b6f7fa82b781350d346a2f6a23938
ashpertine <201197604+ashpertine@users.noreply.github.com>
2026-07-08 00:11:57 +0800

    replace height 100 with flex grow 1

--- client/src/components/HomepageTotalRatingKPI.vue
+++ client/src/components/HomepageTotalRatingKPI.vue
@@ -10,7 +10,7 @@ const totalRatings = computed(() => props.ratings.length);
     <v-card class="d-flex flex-column align-center" height="320px">
       <v-card-title class="text-headline-medium text-medium-emphasis">All Ratings</v-card-title>
       <div></div>
-      <div class="d-flex flex-column align-center justify-center h-100">
+      <div class="d-flex flex-column align-center justify-center flex-grow-1">
         <v-rating :model-value="avgRatings" color="orange-lighten-1" readonly></v-rating>
         <div class="text-display-small"> {{ avgRatings === null ? "No Ratings" : avgRatings }}</div>
         <div class="text-headline-small text-medium-emphasis">Total Ratings: {{ totalRatings }}</div>
